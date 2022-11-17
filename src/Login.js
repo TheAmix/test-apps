88198c1011fb7fcd9cd18912d86133d089363864
TheAmix <amin.sabeti2000@gmail.com>
2022-11-17 05:12:57 +0330

    login page

--- src/Login.js
+++ src/Login.js
@@ -4,8 +4,29 @@ import './Login.css'
 class Login extends Component {
 	render() {
 		return (
-			<h1>hey</h1>
-		);
+			<body>
+   	 			<div class="login">
+      				<div class="avatar">
+        				<img src="avatar.png" alt="avatar img" />
+      				</div>
+      				<h2>Login</h2>
+      				<h3>Welcome back Kelly</h3>
+
+      				<form class="login-form">
+        				<div class="textbox">
+          					<input type="email" placeholder="Username" />
+          					<span class="material-symbols-outlined"></span>
+        				</div>
+        				<div class="textbox">
+          					<input type="password" placeholder="Password" />
+          					<span class="material-symbols-outlined"></span>
+        				</div>
+        				<button type="submit">LOGIN</button>
+        				<a href="https://website.com">Forgot your credentials?</a>
+      				</form>
+    			</div>
+  			</body>
+			);
 	}
 }
 
